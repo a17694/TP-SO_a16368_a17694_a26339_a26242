b32833161a0a25d02b48f213c19b5f38df6a47f1
Joao Ponte <a17694@alunos.ipca.pt>
2023-04-03 21:16:30 +0100

    * Alterações na função de mostrar ficheiros

--- mostrarFicheiro.c
+++ mostrarFicheiro.c
@@ -1,27 +1,39 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int showFile(int argc, char* argv[]) {
+/// @brief Mostra ficheiros
+/// @param argc
+/// @param argv
+void main(int argc, char *argv[])
+{
   char buffer[4096];
   int i, fo, nread, ntotalwritten, nwritten;
 
-    for (i = 1; i < argc; i++) {
-        fo = open(argv[i], O_RDONLY);
-        write(STDOUT_FILENO, argv[i], sizeof(argv[i]));
-        write(STDOUT_FILENO, "#\n", 2);
-        if(fo == -1){
-          write(STDOUT_FILENO, " O ficheiro nao existe\n\n", 26);
+  // @details Loop files
+  for (i = 1; i < argc; i++)
+  {
+    fo = open(argv[i], O_RDONLY);
+    if (fo != -1)
+    {
+      // @detail Read file
+      while ((nread = read(fo, buffer, sizeof(buffer))) > 0)
+      {
+        ntotalwritten = 0;
+        // @details Run file
+        while (ntotalwritten < nread)
+        {
+          nwritten = write(STDOUT_FILENO, buffer + ntotalwritten, nread - ntotalwritten);
+          if (nwritten >= 1) {
+            ntotalwritten += nwritten;
+          }
         }
-        while ((nread = read(fo, buffer, sizeof(buffer))) > 0) {
-            ntotalwritten = 0;
-            while (ntotalwritten < nread) {
-                nwritten = write(STDOUT_FILENO, buffer + ntotalwritten, nread - ntotalwritten);
-                if (nwritten >= 1)
-                  ntotalwritten += nwritten;
-            }
-        }
-        write(STDOUT_FILENO, "\n", 1);
-        close(fo);
+      }
+      write(STDOUT_FILENO, "\n", 1);
+      close(fo);
     }
-    return 0;  
+    else
+    {
+      write(STDERR_FILENO, " O ficheiro nao existe\n\n", 26);
+    }
+  }
 }
