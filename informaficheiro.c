bbb419551ebde7dd525656dc5ca6ce0e645e586f
Joao Ponte <a17694@alunos.ipca.pt>
2023-04-04 19:17:00 +0100

    * add file

---
+++ informaficheiro.c
@@ -0,0 +1,14 @@
+#include <fcntl.h>
+#include <unistd.h>
+
+/// @brief Mostra ficheiros
+/// f) informa ficheiro – Este comando apresenta apenas a informação do sistema
+/// de ficheiros em relação ao ficheiro indicado, tipo de ficheiro (normal,
+/// diretoria, link, etc.), i-node, utilizador dono em formato textual e datas de
+/// criação, leitura e modificação em formato textual;
+/// @param argc
+/// @param argv
+void infofile(int argc, char *argv[])
+{
+
+}
