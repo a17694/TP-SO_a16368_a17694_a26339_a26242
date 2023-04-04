2d7be19c5650deb25f5f782f17d9a3c7c0320dac
Joao Ponte <a17694@alunos.ipca.pt>
2023-04-04 19:54:02 +0100

    * adicionado comentarios

--- informaficheiro.c
+++ informaficheiro.c
@@ -1,13 +1,16 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-/// @brief Mostra ficheiros
-/// f) informa ficheiro – Este comando apresenta apenas a informação do sistema
-/// de ficheiros em relação ao ficheiro indicado, tipo de ficheiro (normal,
-/// diretoria, link, etc.), i-node, utilizador dono em formato textual e datas de
-/// criação, leitura e modificação em formato textual;
-/// @param argc
-/// @param argv
+/**
+ * @brief f) Informa ficheiros
+ * @author a17694 - João Ponte
+ * @details Este comando apresenta apenas a informação do sistema
+ * de ficheiros em relação ao ficheiro indicado, tipo de ficheiro (normal,
+ * diretoria, link, etc.), i-node, utilizador dono em formato textual e datas de
+ * criação, leitura e modificação em formato textual;
+ * @param argc número de parâmetros
+ * @param argv parâmetros
+ */
 void infofile(int argc, char *argv[])
 {
 
