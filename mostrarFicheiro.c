e3d64eb2ad2eb475c19e3f1e7a30e8569fa7fdfe
Joao Ponte <a17694@alunos.ipca.pt>
2023-04-04 20:06:52 +0100

    * Adicionada condição para quando nao for colocado qualque parametro

--- mostrarFicheiro.c
+++ mostrarFicheiro.c
@@ -1,43 +1,45 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-/// @brief Mostra ficheiros
-/// @author a17694 - João Ponte
-/// a) mostra ficheiro – Este comando deve apresentar no ecrã (todo) o conteúdo
-/// do ficheiro indicado como parâmetro. Caso o ficheiro não exista, o comando
-/// deve avisar o utilizador que o ficheiro não existe;
-/// @param argc
-/// @param argv
-void showfile(int argc, char *argv[])
-{
-  char buffer[4096];
-  int i, fo, nread, ntotalwritten, nwritten;
-
-  // @details Loop files
-  for (i = 1; i < argc; i++)
-  {
-    fo = open(argv[i], O_RDONLY);
-    if (fo != -1)
-    {
-      // @detail Read file
-      while ((nread = read(fo, buffer, sizeof(buffer))) > 0)
-      {
-        ntotalwritten = 0;
-        // @details Run file
-        while (ntotalwritten < nread)
-        {
-          nwritten = write(STDOUT_FILENO, buffer + ntotalwritten, nread - ntotalwritten);
-          if (nwritten >= 1) {
-            ntotalwritten += nwritten;
-          }
+/**
+ * @brief a) mostra ficheiro
+ *
+ * @author a17694 - João Ponte
+ *
+ * @details Este comando deve apresentar no ecrã (todo) o conteúdo
+ * do ficheiro indicado como parâmetro. Caso o ficheiro não exista, o comando
+ * deve avisar o utilizador que o ficheiro não existe;
+ *
+ * @param argc número de parâmetros
+ * @param argv parâmetros
+ */
+void showfile(int argc, char *argv[]) {
+    char buffer[4096];
+    int i, fo, nread, ntotalwritten, nwritten;
+    if (argc != 2) {
+        write(STDERR_FILENO, "Erro: Não passou qualquer ficheiro como parâmetro!\n\n", 52);
+        _exit;
+    } else {
+        // Loop files
+        for (i = 1; i < argc; i++) {
+            fo = open(argv[i], O_RDONLY);
+            if (fo != -1) {
+                // Read file
+                while ((nread = read(fo, buffer, sizeof(buffer))) > 0) {
+                    ntotalwritten = 0;
+                    // Run file
+                    while (ntotalwritten < nread) {
+                        nwritten = write(STDOUT_FILENO, buffer + ntotalwritten, nread - ntotalwritten);
+                        if (nwritten >= 1) {
+                            ntotalwritten += nwritten;
+                        }
+                    }
+                }
+                write(STDOUT_FILENO, "\n", 1);
+                close(fo);
+            } else {
+                write(STDERR_FILENO, "Erro: O ficheiro não existe!\n\n", 30);
+            }
         }
-      }
-      write(STDOUT_FILENO, "\n", 1);
-      close(fo);
     }
-    else
-    {
-      write(STDERR_FILENO, "O ficheiro nao existe\n\n", 23);
-    }
-  }
 }
