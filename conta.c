1fc04ce7c1cfe9b704047e19527e40643be7d3b7
hs1lva <a16368@alunos.ipca.pt>
2023-04-17 20:15:50 +0100

    novo ficheiro completamente alterado

--- conta.c
+++ conta.c
@@ -14,54 +14,46 @@
 
 #define MAX_FILE_NAME_LENGTH 256
 
+int main() {
+    char file_name[MAX_FILE_NAME_LENGTH];
+    printf("Digite o nome do arquivo: ");
+    scanf("%s", file_name);
+    count_lines_in_file(file_name);
+    return EXIT_SUCCESS;
+}
+
+
 /**
  * @brief Função que conta o número de linhas em um arquivo.
  *
  * @param file_name Nome do arquivo a ser lido.
  */
-void count_lines_in_file(const char* file_name);
-
-/**
- * @brief Função principal do programa.
- *
- * @param argc Número de argumentos passados na linha de comando.
- * @param argv Array de argumentos passados na linha de comando.
- * @return int Retorna EXIT_SUCCESS em caso de sucesso e EXIT_FAILURE em caso de falha.
- */
-int main(int argc, char* argv[]) {
-    // Verifica se foi passado pelo menos um argumento
-    if (argc < 2) {
-        fprintf(stderr, "Utilização: %s file1 [file2 ...]\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
-    // Percorre todos os argumentos passados na linha de comando
-    for (int i = 1; i < argc; i++) {
-        count_lines_in_file(argv[i]);
-    }
-
-    return EXIT_SUCCESS;
-}
-
 void count_lines_in_file(const char* file_name) {
-    char buffer;
     int file_descriptor;
     int line_count = 0;
+    char prev_char = '\0'; // É um caractere nulo
+    char curr_char;
 
     // Abre o arquivo em modo leitura
     file_descriptor = open(file_name, O_RDONLY);
 
-    // Verifica se houve erro na abertura do arquivo
+    // Verifica se houve erro na abertura em cima
     if (file_descriptor == -1) {
         fprintf(stderr, "Erro ao abrir o arquivo '%s': %s\n", file_name, strerror(errno));
         exit(EXIT_FAILURE);
     }
 
-    // Lê o arquivo um caractere de cada vez
-    while (read(file_descriptor, &buffer, 1) > 0) {
-        if (buffer == '\n') {
+    // Lê um caractere de cada vez
+    while (read(file_descriptor, &curr_char, 1) > 0) {
+        if (curr_char == '\n' && prev_char != '\r') {
             line_count++;
         }
+        prev_char = curr_char;
+    }
+
+    // Verifica se a última linha do arquivo termina em '\n'
+    if (prev_char != '\n') {
+        line_count++;
     }
 
     // Imprime o número de linhas no arquivo
