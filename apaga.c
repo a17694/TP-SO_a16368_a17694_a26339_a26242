494bcb7014dee7261110d987668fe727a2bf37c8
hs1lva <a16368@alunos.ipca.pt>
2023-04-17 20:21:51 +0100

    novo codigo do ficheiro apaga.c

--- apaga.c
+++ apaga.c
@@ -15,34 +15,37 @@
 #define SUCCESS_MESSAGE "Ficheiro '%s' foi eliminado com sucesso\n"
 #define MAX_FILE_NAME_LENGTH 256
 
+ /**
+  * @brief Função principal que recebe os argumentos da linha de comandos
+  * e chama a função para eliminar cada ficheiro
+  *
+  * @param argc número de argumentos
+  * @param argv vetor com os argumentos
+  * @return int retorna EXIT_SUCCESS se tudo correu bem, senão retorna EXIT_FAILURE
+  */
+int main(int argc, char* argv[]) {
+    char file_name[MAX_FILE_NAME_LENGTH];
+
+    if (argc != 1) {
+        fprintf(stderr, "Uso: %s\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Insira o nome do ficheiro a eliminar: ");
+    fgets(file_name, MAX_FILE_NAME_LENGTH, stdin);
+    // Remove o caractere de quebra de linha adicionado pelo fgets
+    file_name[strcspn(file_name, "\n")] = 0;
+
+    delete_file(file_name);
+
+    return EXIT_SUCCESS;
+}
+
 /**
  * @brief Elimina o ficheiro com o nome indicado
  *
  * @param file_name nome do ficheiro a eliminar
  */
-void delete_file(const char* file_name);
-
-/**
- * @brief Função principal que recebe os argumentos da linha de comandos
- * e chama a função para eliminar cada ficheiro
- *
- * @param argc número de argumentos
- * @param argv vetor com os argumentos
- * @return int retorna EXIT_SUCCESS se tudo correu bem, senão retorna EXIT_FAILURE
- */
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        fprintf(stderr, "Uso: %s ficheiro1 [ficheiro2 ...]\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
-    for (int i = 1; i < argc; i++) {
-        delete_file(argv[i]);
-    }
-
-    return EXIT_SUCCESS;
-}
-
 void delete_file(const char* file_name) {
     int result;
 
