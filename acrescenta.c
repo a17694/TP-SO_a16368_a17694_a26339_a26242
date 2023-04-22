2ef9f64691e71b15ddac66603505e07c10c6231c
Pedro Ricardo <pedroricardo10@gmail.com>
2023-04-22 14:26:33 +0100

    está a funcionar

--- acrescenta.c
+++ acrescenta.c
@@ -6,55 +6,34 @@
 
 int main(int argc, char * argv[]){
 
-	int ficheiroOrigem, acrescentaFicheiro,i, leitura, size = 0;
-	struct stat st;
+	int ficheiroOrigem, acrescentaFicheiro;
+    char *linhaOrigem;    
 
 	if(argc <= 2){
         write(STDERR_FILENO, "Algo de errado não está certo, são necessarios dois argumentos\n", 66);
-        return 1;
+        return 0;
     }
 
     ficheiroOrigem = open(argv[1], O_RDONLY);
 	if(ficheiroOrigem <= 0){
-		write(STDERR_FILENO, "Algo de errado não está certo, ficheiro de origem não existe\n", 29);
-        return 1;
+		write(STDERR_FILENO, "Algo de errado não está certo, ficheiro de origem não existe\n", 65);
+        return 0;
 	}
 
-    acrescentaFicheiro = open(argv[2, O_APPEND | O_WRONLY]);
+    acrescentaFicheiro = open(argv[2], O_APPEND | O_WRONLY);
     if(acrescentaFicheiro < 0){
-		write(STDERR_FILENO, "Algo de errado não está certo, ficheiro destino não existe\n", 29);
-        return 1;
+		write(STDERR_FILENO, "Algo de errado não está certo, ficheiro destino não existe\n", 63);
+        return 0;
     }
+	//dar um enter no ficheiro 
+	write(acrescentaFicheiro, "\n", 1);
 
-    char linhaOrigem;
-    int i = 0, j = 0;
+	//escrever caracter a caracter 
     while(read(ficheiroOrigem, &linhaOrigem, 1)){
-        i++;
+        write(acrescentaFicheiro, &linhaOrigem, 1);
     }
-	while(read(acrescentaFicheiro, &linhaOrigem, 1)){
-		ficheiroOrigem[i] = acrescentaFicheiro[j];
-		
-	}
-
-
-    	stat(argv[1], &st);
-        size = st.st_size;
-        char content[size];
-        leitura = read(ficheiroOrigem, content, sizeof(content));      
-	 
-	    acrescentaFicheiro = open (argv[2], O_APPEND | O_WRONLY);
-	
-	if (acrescentaFicheiro == -1){
-		perror("Erro ao abrir o ficheiro");
-		exit(1);
-	}
-
-	else{
-		
-	write(acrescentaFicheiro, content, leitura);
-	}
 
 	close(ficheiroOrigem);
 	close(acrescentaFicheiro);
-	return 0;
+	return 1;
 }
