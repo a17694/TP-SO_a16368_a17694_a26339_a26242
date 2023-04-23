186696db328b6929ffc9f059cd9da50419d7898c
Joao Ponte <a17694@alunos.ipca.pt>
2023-04-23 21:01:25 +0100

    * Finalizado mostr ficheiro

--- mostrarFicheiro.c
+++ mostrarFicheiro.c
@@ -1,45 +1,73 @@
+/*****************************************************************//**
+ * \file   mostrarFicheiro.c
+ * \brief  Ficheiro mostrarFicheiro.c
+ *
+ * \author João Ponte
+ * \date   April 2023
+ *********************************************************************/
+
 #include <fcntl.h>
 #include <unistd.h>
 
 /**
- * @brief a) mostra ficheiro
+* @brief Calcula o comprimento de uma string.
+*
+* @param str A string a ser medida.
+* @return O comprimento da string.
+*/
+int length(char *str) {
+    int len = 0;
+    while (*str != '\0') {
+        len++;
+        str++;
+    }
+    return len;
+}
+
+/**
+ * @brief Mostra o conteúdo de um ou mais ficheiros passados
+ * por parâmetro
  *
- * @author a17694 - João Ponte
- *
- * @details Este comando deve apresentar no ecrã (todo) o conteúdo
+ * @details Este comando apresenta no ecrã o conteúdo
  * do ficheiro indicado como parâmetro. Caso o ficheiro não exista, o comando
- * deve avisar o utilizador que o ficheiro não existe;
+ * avisa o utilizador que o ficheiro não existe;
  *
  * @param argc número de parâmetros
  * @param argv parâmetros
  */
 void showfile(int argc, char *argv[]) {
-    char buffer[4096];
-    int i, fo, nread, ntotalwritten, nwritten;
-    if (argc != 2) {
-        write(STDERR_FILENO, "Erro: Não passou qualquer ficheiro como parâmetro!\n\n", 52);
+    char *linha;
+    int i, ficheiroOrigem;
+
+    // Verifica a quantidade de parâmetro
+    if (argc < 2) {
+        write(STDERR_FILENO, "Erro: Não passou qualquer ficheiro como parâmetro!\n", 52);
         _exit;
     } else {
-        // Loop files
+        // Faz um loop ficheiro a ficheiro passado por parâmetro
         for (i = 1; i < argc; i++) {
-            fo = open(argv[i], O_RDONLY);
-            if (fo != -1) {
-                // Read file
-                while ((nread = read(fo, buffer, sizeof(buffer))) > 0) {
-                    ntotalwritten = 0;
-                    // Run file
-                    while (ntotalwritten < nread) {
-                        nwritten = write(STDOUT_FILENO, buffer + ntotalwritten, nread - ntotalwritten);
-                        if (nwritten >= 1) {
-                            ntotalwritten += nwritten;
-                        }
-                    }
-                }
-                write(STDOUT_FILENO, "\n", 1);
-                close(fo);
-            } else {
-                write(STDERR_FILENO, "Erro: O ficheiro não existe!\n\n", 30);
+
+            // Abre o ficheiro para leitura
+            ficheiroOrigem = open(argv[i], O_RDONLY);
+
+            // Ficheiro não existe
+            if (ficheiroOrigem < 0) {
+                write(STDERR_FILENO, "Erro: ", 6);
+                write(STDERR_FILENO, argv[i], length(argv[i]));
+                write(STDERR_FILENO, " O ficheiro não existe!\n", 25);
+                continue;
             }
+
+            // Lê o conteudo do arquivo caractere por caractere
+            while (read(ficheiroOrigem, &linha, 1)) {
+                write(STDOUT_FILENO, &linha, 1);
+            }
+
+            // Dá uma quebra de linha no final de cada ficheiro
+            write(STDOUT_FILENO, "\n", 1);
+
+            // Fecha o ficheiro
+            close(ficheiroOrigem);
         }
     }
 }
