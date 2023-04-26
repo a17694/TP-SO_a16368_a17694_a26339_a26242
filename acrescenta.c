c73e899e12e2e1cc094a00a0476b2b7cef1cebf2
Pedro Ricardo <pedroricardo10@gmail.com>
2023-04-26 22:59:26 +0100

    Add comentarios

--- acrescenta.c
+++ acrescenta.c
@@ -1,7 +1,16 @@
+/**
+ * @file acrescenta.c
+ * @author Pedro Silva (a26342@alunos.ipca.pt)
+ * @brief Acrescenta conteudo de um ficheiro a outro
+ * @version 0.1
+ * @date 2023-04
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
 #include <fcntl.h>
 #include <unistd.h>
-//#include <stdio.h>
-//#include <stdlib.h>
 #include <sys/stat.h>
 
 int main(int argc, char * argv[]){
@@ -18,12 +27,13 @@ int main(int argc, char * argv[]){
 			write(STDERR_FILENO, "Algo de errado não está certo, ficheiro de origem não existe\n", 65);
 			return 0;
 		}
-
+		//abre o ficheiro de destico como append, para adicionar texto no final.
 		acrescentaFicheiro = open(argv[2], O_APPEND | O_WRONLY);
 		if(acrescentaFicheiro < 0){
 			write(STDERR_FILENO, "Algo de errado não está certo, ficheiro destino não existe\n", 63);
 			return 0;
 		}
+
 		//dar um enter no ficheiro 
 		write(acrescentaFicheiro, "\n", 1);
 
@@ -31,8 +41,6 @@ int main(int argc, char * argv[]){
 		while(read(ficheiroOrigem, &linhaOrigem, 1)){
 			write(acrescentaFicheiro, &linhaOrigem, 1);
 		}
-	
-
 	close(ficheiroOrigem);
 	close(acrescentaFicheiro);
 	return 1;
